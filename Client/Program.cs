b2324ad1dacd75495a1ffcf141d5509b4f8118ca
Alex Noddings <alexnoddings@users.noreply.github.com>
2020-06-28 18:13:25 +0100

    Update client hosting method.

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,32 +1,34 @@
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace Quibble.Client
 {
     public static class Program
     {
-        public static async Task Main(string[] args)
+        public static async Task Main(string[] args) =>
+            await CreateHostBuilder(args).Build().RunAsync().ConfigureAwait(false);
+
+        public static WebAssemblyHostBuilder CreateHostBuilder(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
+            ConfigureServices(builder.Services, builder.HostEnvironment);
+            return builder;
+        }
 
-            builder.Services.AddHttpClient("Quibble.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+        public static void ConfigureServices(IServiceCollection services, IWebAssemblyHostEnvironment env)
+        {
+            services.AddHttpClient("Quibble.ServerAPI", client => client.BaseAddress = new Uri(env.BaseAddress))
                 .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
             // Supply HttpClient instances that include access tokens when making requests to the server project
-            builder.Services.AddTransient(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Quibble.ServerAPI"));
+            services.AddTransient(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Quibble.ServerAPI"));
 
-            builder.Services.AddApiAuthorization();
-
-            await builder.Build().RunAsync().ConfigureAwait(false);
+            services.AddApiAuthorization();
         }
     }
 }
