c0e76230509a94e564872bf9780252271ee37661
Alex Noddings <alexnoddings@users.noreply.github.com>
2020-07-01 15:29:07 +0100

    Change UserSettings to use string for UserId.

--- Server/Models/Users/UserSettings.cs
+++ Server/Models/Users/UserSettings.cs
@@ -5,7 +5,7 @@ namespace Quibble.Server.Models.Users
     public class UserSettings : IEntity<Guid>
     {
         public Guid Id { get; }
-        public Guid UserId { get; set; }
+        public string UserId { get; set; } = string.Empty;
 
         public bool UseNightMode { get; set; }
     }
