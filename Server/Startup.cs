247af3e1f0205a43e85c7d87b6a8da59f78bd4e6
Alex Noddings <alexnoddings@users.noreply.github.com>
2020-07-02 12:59:37 +0100

    Update comment.

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -92,7 +92,7 @@ namespace Quibble.Server
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // Defaults to 30 days.
                 app.UseHsts();
             }
 
