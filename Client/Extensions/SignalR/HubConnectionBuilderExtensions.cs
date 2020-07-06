2001cba8fcc7b3d1a219a101a18fd2330fd34ff9
Alex Noddings <alexnoddings@users.noreply.github.com>
2020-07-06 15:01:24 +0100

    Change SignalR hub connection builder extensions to make it clearer whether a relative/absolute URI is needed/used.

--- Client/Extensions/SignalR/HubConnectionBuilderExtensions.cs
+++ Client/Extensions/SignalR/HubConnectionBuilderExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Http.Connections.Client;
 using Microsoft.AspNetCore.SignalR.Client;
@@ -9,33 +10,20 @@ namespace Quibble.Client.Extensions.SignalR
     /// <summary>
     /// Extension methods for <see cref="IHubConnectionBuilder"/>.
     /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1054:Uri parameters should not be strings", Justification = "Uri parameters are exposed as strings for ease of use. They are all converted into Uris before use.")]
     public static class HubConnectionBuilderExtensions
     {
-        /// <inheritdoc cref="WithAuthenticatedUrlCore"/>
+        /// <summary>
+        /// Configures the <see cref="HubConnection"/> to use access token authenticated HTTP-based transports to connect to the specified URL.
+        /// </summary>
+        /// <param name="hubConnectionBuilder">The <see cref="IHubConnectionBuilder" /> to configure.</param>
+        /// <param name="url">The URL the <see cref="HttpConnection"/> will use.</param>
+        /// <param name="accessTokenProvider">A service which provides an <see cref="AccessToken"/>.</param>
+        /// <returns>The same instance of the <see cref="IHubConnectionBuilder"/> for chaining.</returns>
+        /// <remarks>See <see cref="HubConnectionBuilderHttpExtensions.WithUrl(IHubConnectionBuilder, Uri, Action{HttpConnectionOptions})"/></remarks>
         public static IHubConnectionBuilder WithAuthenticatedUrl(this IHubConnectionBuilder hubConnectionBuilder, Uri url, IAccessTokenProvider accessTokenProvider)
         {
             hubConnectionBuilder.WithAuthenticatedUrlCore(url, accessTokenProvider, null);
-            return hubConnectionBuilder;
-        }
-
-        /// <inheritdoc cref="WithAuthenticatedUrlCore"/>
-        public static IHubConnectionBuilder WithAuthenticatedUrl(this IHubConnectionBuilder hubConnectionBuilder, Uri url, IAccessTokenProvider accessTokenProvider, Action<HttpConnectionOptions>? configureHttpConnection)
-        {
-            hubConnectionBuilder.WithAuthenticatedUrlCore(url, accessTokenProvider, configureHttpConnection);
-            return hubConnectionBuilder;
-        }
-
-        /// <inheritdoc cref="WithAuthenticatedUrlCore"/>
-        public static IHubConnectionBuilder WithAuthenticatedUrl(this IHubConnectionBuilder hubConnectionBuilder, string url, IAccessTokenProvider accessTokenProvider)
-        {
-            hubConnectionBuilder.WithAuthenticatedUrlCore(new Uri(url), accessTokenProvider, null);
-            return hubConnectionBuilder;
-        }
-
-        /// <inheritdoc cref="WithAuthenticatedUrlCore"/>
-        public static IHubConnectionBuilder WithAuthenticatedUrl(this IHubConnectionBuilder hubConnectionBuilder, string url, IAccessTokenProvider accessTokenProvider, Action<HttpConnectionOptions>? configureHttpConnection)
-        {
-            hubConnectionBuilder.WithAuthenticatedUrlCore(new Uri(url), accessTokenProvider, configureHttpConnection);
             return hubConnectionBuilder;
         }
 
@@ -43,7 +31,105 @@ namespace Quibble.Client.Extensions.SignalR
         /// Configures the <see cref="HubConnection"/> to use access token authenticated HTTP-based transports to connect to the specified URL.
         /// </summary>
         /// <param name="hubConnectionBuilder">The <see cref="IHubConnectionBuilder" /> to configure.</param>
-        /// <param name="url">The URI the <see cref="HttpConnection"/> will use.</param>
+        /// <param name="url">The URL the <see cref="HttpConnection"/> will use.</param>
+        /// <param name="accessTokenProvider">A service which provides an <see cref="AccessToken"/>.</param>
+        /// <param name="configureHttpConnection">The delegate that configures the <see cref="HttpConnection"/>.</param>
+        /// <returns>The same instance of the <see cref="IHubConnectionBuilder"/> for chaining.</returns>
+        /// <remarks>See <see cref="HubConnectionBuilderHttpExtensions.WithUrl(IHubConnectionBuilder, Uri, Action{HttpConnectionOptions})"/></remarks>
+        public static IHubConnectionBuilder WithAuthenticatedUrl(this IHubConnectionBuilder hubConnectionBuilder, Uri url, IAccessTokenProvider accessTokenProvider, Action<HttpConnectionOptions>? configureHttpConnection)
+        {
+            hubConnectionBuilder.WithAuthenticatedUrlCore(url, accessTokenProvider, configureHttpConnection);
+            return hubConnectionBuilder;
+        }
+
+        /// <summary>
+        /// Configures the <see cref="HubConnection"/> to use access token authenticated HTTP-based transports to connect to the specified URL.
+        /// </summary>
+        /// <param name="hubConnectionBuilder">The <see cref="IHubConnectionBuilder" /> to configure.</param>
+        /// <param name="absoluteUrl">The absolute URL the <see cref="HttpConnection"/> will use. For relative URLs, see <see cref="WithAuthenticatedRelativeUrl(IHubConnectionBuilder,NavigationManager,string,IAccessTokenProvider)"/>.</param>
+        /// <param name="accessTokenProvider">A service which provides an <see cref="AccessToken"/>.</param>
+        /// <returns>The same instance of the <see cref="IHubConnectionBuilder"/> for chaining.</returns>
+        /// <remarks>See <see cref="HubConnectionBuilderHttpExtensions.WithUrl(IHubConnectionBuilder, Uri, Action{HttpConnectionOptions})"/></remarks>
+        public static IHubConnectionBuilder WithAuthenticatedAbsoluteUrl(this IHubConnectionBuilder hubConnectionBuilder, string absoluteUrl, IAccessTokenProvider accessTokenProvider)
+        {
+            if (absoluteUrl == null) throw new ArgumentNullException(nameof(absoluteUrl));
+
+            var absoluteUrlAsUri = new Uri(absoluteUrl);
+            if (absoluteUrlAsUri.Scheme != "http" && absoluteUrlAsUri.Scheme != "https")
+                throw new ArgumentException("Only http and https schemes are supported.");
+
+            hubConnectionBuilder.WithAuthenticatedUrlCore(absoluteUrlAsUri, accessTokenProvider, null);
+            return hubConnectionBuilder;
+        }
+
+        /// <summary>
+        /// Configures the <see cref="HubConnection"/> to use access token authenticated HTTP-based transports to connect to the specified URL.
+        /// </summary>
+        /// <param name="hubConnectionBuilder">The <see cref="IHubConnectionBuilder" /> to configure.</param>
+        /// <param name="absoluteUrl">The absolute URL the <see cref="HttpConnection"/> will use. For relative URLs, see <see cref="WithAuthenticatedRelativeUrl(IHubConnectionBuilder,NavigationManager,string,IAccessTokenProvider,Action{HttpConnectionOptions})"/>.</param>
+        /// <param name="accessTokenProvider">A service which provides an <see cref="AccessToken"/>.</param>
+        /// <param name="configureHttpConnection">The delegate that configures the <see cref="HttpConnection"/>.</param>
+        /// <returns>The same instance of the <see cref="IHubConnectionBuilder"/> for chaining.</returns>
+        /// <remarks>See <see cref="HubConnectionBuilderHttpExtensions.WithUrl(IHubConnectionBuilder, Uri, Action{HttpConnectionOptions})"/></remarks>
+        public static IHubConnectionBuilder WithAuthenticatedAbsoluteUrl(this IHubConnectionBuilder hubConnectionBuilder, string absoluteUrl, IAccessTokenProvider accessTokenProvider, Action<HttpConnectionOptions>? configureHttpConnection)
+        {
+            if (absoluteUrl == null) throw new ArgumentNullException(nameof(absoluteUrl));
+
+            var absoluteUrlAsUri = new Uri(absoluteUrl);
+            if (absoluteUrlAsUri.Scheme != "http" && absoluteUrlAsUri.Scheme != "https")
+                throw new ArgumentException("Only http and https schemes are supported.");
+
+            hubConnectionBuilder.WithAuthenticatedUrlCore(absoluteUrlAsUri, accessTokenProvider, configureHttpConnection);
+            return hubConnectionBuilder;
+        }
+
+        /// <summary>
+        /// Configures the <see cref="HubConnection"/> to use access token authenticated HTTP-based transports to connect to the specified URL.
+        /// </summary>
+        /// <param name="hubConnectionBuilder">The <see cref="IHubConnectionBuilder" /> to configure.</param>
+        /// <param name="navigationManager">A <see cref="NavigationManager"/> to find the absolute url.</param>
+        /// <param name="relativeUrl">A relative URL the <see cref="HttpConnection"/> will use. This is made absolute by the <paramref name="navigationManager"/>. For absolute URLs, see <see cref="WithAuthenticatedAbsoluteUrl(IHubConnectionBuilder,string,IAccessTokenProvider)"/></param>
+        /// <param name="accessTokenProvider">A service which provides an <see cref="AccessToken"/>.</param>
+        /// <returns>The same instance of the <see cref="IHubConnectionBuilder"/> for chaining.</returns>
+        /// <remarks>See <see cref="HubConnectionBuilderHttpExtensions.WithUrl(IHubConnectionBuilder, Uri, Action{HttpConnectionOptions})"/></remarks>
+        public static IHubConnectionBuilder WithAuthenticatedRelativeUrl(this IHubConnectionBuilder hubConnectionBuilder, NavigationManager navigationManager, string relativeUrl, IAccessTokenProvider accessTokenProvider)
+        {
+            if (relativeUrl == null) throw new ArgumentNullException(nameof(relativeUrl));
+            if (navigationManager == null) throw new ArgumentNullException(nameof(navigationManager));
+
+            Uri absoluteUrl = navigationManager.ToAbsoluteUri(relativeUrl);
+
+            hubConnectionBuilder.WithAuthenticatedUrlCore(absoluteUrl, accessTokenProvider, null);
+            return hubConnectionBuilder;
+        }
+
+
+        /// <summary>
+        /// Configures the <see cref="HubConnection"/> to use access token authenticated HTTP-based transports to connect to the specified URL.
+        /// </summary>
+        /// <param name="hubConnectionBuilder">The <see cref="IHubConnectionBuilder" /> to configure.</param>
+        /// <param name="navigationManager">A <see cref="NavigationManager"/> to find the absolute url.</param>
+        /// <param name="relativeUrl">A relative URL the <see cref="HttpConnection"/> will use. This is made absolute by the <paramref name="navigationManager"/>. For absolute URLs, see <see cref="WithAuthenticatedAbsoluteUrl(IHubConnectionBuilder,string,IAccessTokenProvider,Action{HttpConnectionOptions})"/></param>
+        /// <param name="accessTokenProvider">A service which provides an <see cref="AccessToken"/>.</param>
+        /// <param name="configureHttpConnection">The delegate that configures the <see cref="HttpConnection"/>.</param>
+        /// <returns>The same instance of the <see cref="IHubConnectionBuilder"/> for chaining.</returns>
+        /// <remarks>See <see cref="HubConnectionBuilderHttpExtensions.WithUrl(IHubConnectionBuilder, Uri, Action{HttpConnectionOptions})"/></remarks>
+        public static IHubConnectionBuilder WithAuthenticatedRelativeUrl(this IHubConnectionBuilder hubConnectionBuilder, NavigationManager navigationManager, string relativeUrl, IAccessTokenProvider accessTokenProvider, Action<HttpConnectionOptions>? configureHttpConnection)
+        {
+            if (relativeUrl == null) throw new ArgumentNullException(nameof(relativeUrl));
+            if (navigationManager == null) throw new ArgumentNullException(nameof(navigationManager));
+
+            Uri absoluteUrl = navigationManager.ToAbsoluteUri(relativeUrl);
+
+            hubConnectionBuilder.WithAuthenticatedUrlCore(absoluteUrl, accessTokenProvider, configureHttpConnection);
+            return hubConnectionBuilder;
+        }
+
+        /// <summary>
+        /// Configures the <see cref="HubConnection"/> to use access token authenticated HTTP-based transports to connect to the specified URL.
+        /// </summary>
+        /// <param name="hubConnectionBuilder">The <see cref="IHubConnectionBuilder" /> to configure.</param>
+        /// <param name="url">The URL the <see cref="HttpConnection"/> will use.</param>
         /// <param name="accessTokenProvider">A service which provides an <see cref="AccessToken"/>.</param>
         /// <param name="configureHttpConnection">The delegate that configures the <see cref="HttpConnection"/>.</param>
         /// <returns>The same instance of the <see cref="IHubConnectionBuilder"/> for chaining.</returns>
