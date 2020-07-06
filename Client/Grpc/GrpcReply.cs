2bca4cd261a636ba51e0c0cbd0569a14801f55a7
Alex Noddings <alexnoddings@users.noreply.github.com>
2020-07-06 14:58:32 +0100

    Add a GrpcReply class for exposing gRPC call status easier.

---
+++ Client/Grpc/GrpcReply.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Grpc.Core;
+
+namespace Quibble.Client.Grpc
+{
+    public class GrpcReply
+    {
+        public StatusCode StatusCode { get; }
+
+        public bool OK => StatusCode == StatusCode.OK;
+
+        public string StatusDetail { get; }
+
+        public GrpcReply(StatusCode statusCode, string statusDetail)
+        {
+            StatusCode = statusCode;
+            StatusDetail = statusDetail ?? throw new ArgumentNullException(nameof(statusDetail));
+        }
+    }
+
+    public class GrpcReply<TReply> : GrpcReply
+        where TReply : class
+    {
+        public TReply Value { get; }
+
+        public GrpcReply(StatusCode statusCode, string statusDetail, TReply value) 
+            : base(statusCode, statusDetail)
+        {
+            Value = value ?? throw new ArgumentNullException(nameof(value));
+        }
+    }
+}
