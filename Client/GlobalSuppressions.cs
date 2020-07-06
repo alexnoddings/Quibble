591018f95e9e4154f3fe6577626f4a487c96938a
Alex Noddings <alexnoddings@users.noreply.github.com>
2020-07-06 15:08:26 +0100

    Add suppression to client to ignore a warning which would cause an error if "fixed".

---
+++ Client/GlobalSuppressions.cs
@@ -0,0 +1,3 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+[assembly:SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "Component parameters (e.g. attributes) require public setters.", Scope = "namespaceanddescendants", Target = "Quibble.Client.Components")] 
