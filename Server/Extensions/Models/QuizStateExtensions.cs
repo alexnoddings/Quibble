d5363a29d63caeb3e103fe06d495bc321e3d1f3d
Alex Noddings <alexnoddings@users.noreply.github.com>
2020-07-06 14:57:23 +0100

    Add extension for QuizState for easier switching between model and proto QuizState.

---
+++ Server/Extensions/Models/QuizStateExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using ProtosQuizState = Quibble.Common.Protos.QuizState;
+using ModelsQuizState = Quibble.Server.Models.Quizzes.QuizState;
+
+namespace Quibble.Server.Extensions.Models
+{
+    /// <summary>
+    /// Extension methods for converting between <see cref="ProtosQuizState"/> and <see cref="ModelsQuizState"/>.
+    /// </summary>
+    public static class QuizStateExtensions
+    {
+        /// <summary>
+        /// Convert to a <see cref="ProtosQuizState"/>.
+        /// </summary>
+        /// <param name="state">The state.</param>
+        /// <returns>The converted state.</returns>
+        public static ProtosQuizState ToProtoEnum(this ModelsQuizState state) =>
+            state switch
+            {
+                ModelsQuizState.Closed => ProtosQuizState.Closed,
+                ModelsQuizState.InDevelopment => ProtosQuizState.InDevelopment,
+                ModelsQuizState.InProgress => ProtosQuizState.InProgress,
+                ModelsQuizState.WaitingForPlayers => ProtosQuizState.WaitingForPlayers,
+                _ => throw new ArgumentOutOfRangeException(nameof(state))
+            };
+
+        /// <summary>
+        /// Convert to a <see cref="ModelsQuizState"/>.
+        /// </summary>
+        /// <param name="state">The state.</param>
+        /// <returns>The converted state.</returns>
+        public static ModelsQuizState ToModelEnum(this ProtosQuizState state) =>
+            state switch
+            {
+                ProtosQuizState.Closed => ModelsQuizState.Closed,
+                ProtosQuizState.InDevelopment => ModelsQuizState.InDevelopment,
+                ProtosQuizState.InProgress => ModelsQuizState.InProgress,
+                ProtosQuizState.WaitingForPlayers => ModelsQuizState.WaitingForPlayers,
+                _ => throw new ArgumentOutOfRangeException(nameof(state))
+            };
+    }
+}
