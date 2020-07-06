b7ab096216aa61682253b9a506d21ab2ddc5c5e8
Alex Noddings <alexnoddings@users.noreply.github.com>
2020-07-06 14:49:36 +0100

    Add a global suppression to server for not checking GRPC service arguments.

--- Server/GlobalSuppressions.cs
+++ Server/GlobalSuppressions.cs
@@ -2,7 +2,8 @@
 
 [assembly: SuppressMessage("Design", "CA1054:Uri parameters should not be strings", Justification = "Uri parameters should be strings for Identity pages.", Scope = "namespaceanddescendants", Target = "Quibble.Server.Areas.Identity")]
 [assembly: SuppressMessage("Design", "CA1056:Uri properties should not be strings", Justification = "Uri properties should be strings for Identity pages.", Scope = "namespaceanddescendants", Target = "Quibble.Server.Areas.Identity")]
-[assembly: SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "Not necessary for migrations", Scope = "namespaceanddescendants", Target = "Quibble.Server.Data.Migrations")]
+[assembly: SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "Not necessary for migrations.", Scope = "namespaceanddescendants", Target = "Quibble.Server.Data.Migrations")]
+[assembly: SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "Arguments are handled by GRPC and are never null.", Scope = "namespaceanddescendants", Target = "Quibble.Server.Grpc")]
 
 [assembly: SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "Empty members are useful for some Identity pages.", Scope = "namespaceanddescendants", Target = "Quibble.Server.Areas.Identity")]
 
