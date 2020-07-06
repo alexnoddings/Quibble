34ed8cb853af9672abb7bdae621a6dd0a651ea59
Alex Noddings <alexnoddings@users.noreply.github.com>
2020-07-06 23:16:18 +0100

    Add interfaces for the SignalR quiz hub.

--- Common/SignalR/IQuizHubClient.cs
+++ Common/SignalR/IQuizHubClient.cs
@@ -5,13 +5,13 @@ namespace Quibble.Common.SignalR
     /// <summary>
     /// Provides the actions available to perform on clients of the Quiz SignalR hub.
     /// </summary>
-    public interface IQuizHub
+    public interface IQuizHubClient
     {
         /// <summary>
         /// Called when a Quiz's title is updated.
         /// </summary>
         /// <param name="newTitle">The new title.</param>
-        /// <returns>A task that represents the asynchronous operation.</returns>
+        /// <returns>A <see cref="Task"/> that represents the asynchronous updating operation.</returns>
         Task OnQuizTitleUpdated(string newTitle);
     }
 }
