1630c0926d12dc62921155d0011cef1865d4be96
Alex Noddings <alexnoddings@users.noreply.github.com>
2020-07-06 15:03:41 +0100

    Add create quiz page to client.

---
+++ Client/Pages/Quizzes/CreateQuiz.razor.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Quibble.Client.Extensions.Grpc;
+
+namespace Quibble.Client.Pages.Quizzes
+{
+    public partial class CreateQuiz
+    {
+        private string NewQuizTitle { get; set; } = string.Empty;
+
+        private string? ErrorDetail { get; set; } = null;
+
+        private async Task CreateQuizAsync()
+        {
+            var reply = await QuizClient
+                .CreateAsync(NewQuizTitle)
+                .ConfigureAwait(false);
+
+            if (reply.OK)
+            {
+                NavigationManager.NavigateTo($"/quiz/{reply.Value.Id}");
+                return;
+            }
+
+            ErrorDetail = reply.StatusDetail;
+        }
+    }
+}
