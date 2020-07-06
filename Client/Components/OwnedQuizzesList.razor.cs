5c6241fdec5f6217957b7c27bb3a0e9d86e8faf1
Alex Noddings <alexnoddings@users.noreply.github.com>
2020-07-06 15:03:27 +0100

    Add owned quizzes list to index.

---
+++ Client/Components/OwnedQuizzesList.razor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Quibble.Client.Extensions.Grpc;
+using Quibble.Common.Protos;
+
+namespace Quibble.Client.Components
+{
+    public partial class OwnedQuizzesList
+    {
+        private List<QuizInfo> Quizzes { get; } = new List<QuizInfo>();
+
+        private string? Error { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync().ConfigureAwait(false);
+
+            var ownedQuizzesReply = await QuizClient.GetOwnedAsync().ConfigureAwait(false);
+
+            if (ownedQuizzesReply.OK)
+            {
+                Quizzes.Clear();
+                var infos = ownedQuizzesReply.Value.QuizInfos;
+                if (infos.Count == 0)
+                {
+                    Error = "No quizzes found";
+                }
+                else
+                {
+                    Error = null;
+                    Quizzes.AddRange(infos);
+                }
+            }
+            else
+            {
+                Error = ownedQuizzesReply.StatusDetail;
+            }
+        }
+    }
+}
---
+++ Client/Pages/Index.razor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Quibble.Client.Extensions.Grpc;
+
+namespace Quibble.Client.Pages
+{
+    public partial class Index
+    {
+        private List<(Guid, string)> OwnedQuizzes { get; } = new List<(Guid, string)>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync().ConfigureAwait(false);
+
+            var quizzesReply = await QuizClient.GetOwnedAsync().ConfigureAwait(false);
+            if (quizzesReply.OK)
+            {
+                foreach (var quiz in quizzesReply.Value.QuizInfos)
+                {
+                    Guid id = Guid.Parse(quiz.Id);
+                    OwnedQuizzes.Add((id, quiz.Title));
+                }
+            }
+            else
+            {
+                Console.WriteLine($"err {quizzesReply.StatusCode}: {quizzesReply.StatusDetail}");
+            }
+        }
+    }
+}
