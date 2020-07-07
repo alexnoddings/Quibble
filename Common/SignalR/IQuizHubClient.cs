855b1f354a261cfa5544be2200e8c9211beb8793
Alex Noddings <alexnoddings@users.noreply.github.com>
2020-07-07 14:58:57 +0100

    Update quiz hub client to support more update events.

--- Common/SignalR/IQuizHubClient.cs
+++ Common/SignalR/IQuizHubClient.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+using Quibble.Common.Protos;
 
 namespace Quibble.Common.SignalR
 {
@@ -8,10 +9,27 @@ namespace Quibble.Common.SignalR
     public interface IQuizHubClient
     {
         /// <summary>
-        /// Called when a Quiz's title is updated.
+        /// Called when a Quiz is updated.
         /// </summary>
         /// <param name="newTitle">The new title.</param>
         /// <returns>A <see cref="Task"/> that represents the asynchronous updating operation.</returns>
-        Task OnQuizTitleUpdated(string newTitle);
+        Task OnQuizUpdated(string newTitle);
+
+        /// <summary>
+        /// Called when a Round is updated.
+        /// </summary>
+        /// <param name="newTitle">The new title.</param>
+        /// <param name="newState">The new state.</param>
+        /// <returns>A <see cref="Task"/> that represents the asynchronous updating operation.</returns>
+        Task OnRoundUpdated(string newTitle, RoundState newState);
+
+        /// <summary>
+        /// Called when a Question is updated.
+        /// </summary>
+        /// <param name="newBody">The new body.</param>
+        /// <param name="newAnswer">The new answer.</param>
+        /// <param name="newState">The new state.</param>
+        /// <returns>A <see cref="Task"/> that represents the asynchronous updating operation.</returns>
+        Task OnQuestionUpdated(string newBody, string newAnswer, QuestionState newState);
     }
 }
