199bfe284ee8378a8ea5a8433fc264da14ae9987
Alex Noddings <alexnoddings@users.noreply.github.com>
2020-07-07 14:46:24 +0100

    Add server question service.

---
+++ Server/Grpc/QuestionService.cs
@@ -0,0 +1,24 @@
+﻿using Quibble.Common.Protos;
+using Quibble.Server.Extensions.Models;
+using Quibble.Server.Models.Questions;
+
+namespace Quibble.Server.Grpc
+{
+    public class QuestionService : Common.Protos.QuestionService.QuestionServiceBase
+    {
+        /// <summary>
+        /// Converts a <see cref="Question"/> to a <see cref="QuestionInfo"/>.
+        /// </summary>
+        /// <param name="question">The <see cref="Question"/> to convert.</param>
+        /// <returns>The converted <see cref="QuestionInfo"/>.</returns>
+        internal static QuestionInfo ToQuestionInfo(Question question) =>
+            new QuestionInfo
+            {
+                Id = question.Id.ToString(),
+                RoundId = question.RoundId.ToString(),
+                Body = question.Body,
+                Answer = question.Answer,
+                State = question.State.ToProtoEnum()
+            };
+    }
+}
