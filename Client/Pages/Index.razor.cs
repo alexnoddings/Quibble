53d1f8d6c0153a92cc053bf1c8d6dc3fcf38b206
Alex Noddings <alexnoddings@users.noreply.github.com>
2020-07-07 14:20:53 +0100

    Remove unused index code from client.

--- Client/Pages/Index.razor.cs
+++ Client/Pages/Index.razor.cs
@@ -1,31 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Quibble.Client.Extensions.Grpc;
-
-namespace Quibble.Client.Pages
+﻿namespace Quibble.Client.Pages
 {
     public partial class Index
     {
-        private List<(Guid, string)> OwnedQuizzes { get; } = new List<(Guid, string)>();
-
-        protected override async Task OnInitializedAsync()
-        {
-            await base.OnInitializedAsync().ConfigureAwait(false);
-
-            var quizzesReply = await QuizClient.GetOwnedAsync().ConfigureAwait(false);
-            if (quizzesReply.Ok)
-            {
-                foreach (var quiz in quizzesReply.Value.QuizInfos)
-                {
-                    Guid id = Guid.Parse(quiz.Id);
-                    OwnedQuizzes.Add((id, quiz.Title));
-                }
-            }
-            else
-            {
-                Console.WriteLine($"err {quizzesReply.StatusCode}: {quizzesReply.StatusDetail}");
-            }
-        }
     }
 }
