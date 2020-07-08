ed3b845b5a2c24c4e05f37a056c6c8a82f1acabe
Alex Noddings <alexnoddings@users.noreply.github.com>
2020-07-08 11:34:03 +0100

    Move server entity configuration into seperate files.

---
+++ Server/Data/EntityConfigurations/UserSettingsEntityConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Quibble.Server.Models.Users;
+
+namespace Quibble.Server.Data.EntityConfigurations
+{
+    /// <summary>
+    /// Provides entity configuration for <see cref="UserSettings"/>.
+    /// </summary>
+    public class UserSettingsEntityConfiguration : IEntityTypeConfiguration<UserSettings>
+    {
+        /// <inheritdoc />
+        public void Configure(EntityTypeBuilder<UserSettings> builder)
+        {
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+            builder.HasKey(userSettings => userSettings.Id);
+
+            builder
+                .HasOne<ApplicationUser>()
+                .WithMany()
+                .HasForeignKey(userSettings => userSettings.UserId)
+                .IsRequired();
+
+            builder.ToTable("AspNetUserSettings");
+        }
+    }
+}
