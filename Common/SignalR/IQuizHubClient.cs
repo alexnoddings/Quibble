f8d57de666cb741046e64e0e7b9ec1ac66d4e512
Alex Noddings <alexnoddings@users.noreply.github.com>
2020-07-10 11:06:30 +0100

    Add more actions to the quiz hub.

--- Common/SignalR/IQuizHubClient.cs
+++ Common/SignalR/IQuizHubClient.cs
@@ -11,25 +11,58 @@ namespace Quibble.Common.SignalR
         /// <summary>
         /// Called when a Quiz is updated.
         /// </summary>
-        /// <param name="newTitle">The new title.</param>
-        /// <returns>A <see cref="Task"/> that represents the asynchronous updating operation.</returns>
-        Task OnQuizUpdated(string newTitle);
+        /// <param name="quizInfo">The updated <see cref="QuizInfo"/>.</param>
+        /// <returns>A <see cref="Task"/> that represents the asynchronous operation.</returns>
+        Task OnQuizUpdated(QuizInfo quizInfo);
+
+        /// <summary>
+        /// Called when a Quiz is deleted.
+        /// </summary>
+        /// <param name="quizId">The identifier of the Quiz.</param>
+        /// <returns>A <see cref="Task"/> that represents the asynchronous operation.</returns>
+        Task OnQuizDeleted(string quizId);
+
+        /// <summary>
+        /// Called when a new Round is created.
+        /// </summary>
+        /// <param name="roundInfo">The updated <see cref="RoundInfo"/>.</param>
+        /// <returns>A <see cref="Task"/> that represents the asynchronous operation.</returns>
+        Task OnRoundCreated(RoundInfo roundInfo);
 
         /// <summary>
         /// Called when a Round is updated.
         /// </summary>
-        /// <param name="newTitle">The new title.</param>
-        /// <param name="newState">The new state.</param>
-        /// <returns>A <see cref="Task"/> that represents the asynchronous updating operation.</returns>
-        Task OnRoundUpdated(string newTitle, RoundState newState);
+        /// <param name="roundInfo">The updated <see cref="RoundInfo"/>.</param>
+        /// <returns>A <see cref="Task"/> that represents the asynchronous operation.</returns>
+        Task OnRoundUpdated(RoundInfo roundInfo);
+
+        /// <summary>
+        /// Called when a Round is deleted.
+        /// </summary>
+        /// <param name="roundId">The identifier of the Round.</param>
+        /// <returns>A <see cref="Task"/> that represents the asynchronous operation.</returns>
+        Task OnRoundDeleted(string roundId);
+
+        /// <summary>
+        /// Called when a new Question is created.
+        /// </summary>
+        /// <param name="questionInfo">The updated <see cref="QuestionInfo"/>.</param>
+        /// <returns>A <see cref="Task"/> that represents the asynchronous operation.</returns>
+        Task OnQuestionCreated(QuestionInfo questionInfo);
 
         /// <summary>
         /// Called when a Question is updated.
         /// </summary>
-        /// <param name="newBody">The new body.</param>
-        /// <param name="newAnswer">The new answer.</param>
-        /// <param name="newState">The new state.</param>
-        /// <returns>A <see cref="Task"/> that represents the asynchronous updating operation.</returns>
-        Task OnQuestionUpdated(string newBody, string newAnswer, QuestionState newState);
+        /// <param name="questionInfo">The updated <see cref="QuestionInfo"/>.</param>
+        /// <returns>A <see cref="Task"/> that represents the asynchronous operation.</returns>
+        Task OnQuestionUpdated(QuestionInfo questionInfo);
+
+        /// <summary>
+        /// Called when a Question is deleted.
+        /// </summary>
+        /// <param name="roundId">The identifier of the Question's parent Round.</param>
+        /// <param name="questionId">The identifier of the Question.</param>
+        /// <returns>A <see cref="Task"/> that represents the asynchronous operation.</returns>
+        Task OnQuestionDeleted(string roundId, string questionId);
     }
 }
