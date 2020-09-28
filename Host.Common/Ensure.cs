8713df836468cb7dcf5cf1d83dd84d08a22cff3d
Alex Noddings <alexnoddings@users.noreply.github.com>
2020-09-28 19:27:56 +0100

    Add Ensure for precondition checking.

---
+++ Host.Common/Ensure.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Contracts;
+
+namespace Quibble.Host.Common
+{
+    [Pure]
+    public static class Ensure
+    {
+        public static void NotNullOrDefault<T>([AllowNull] T? obj, string paramName, string? message = null)
+        {
+            if (obj is null)
+                throw ThrowHelper.NullArgument(paramName, message);
+            if (obj.Equals(default(T)))
+                throw ThrowHelper.BadArgument(paramName, message);
+        }
+
+        public static void Found([AllowNull] object? obj, string typeName, object? id = null, string? message = null)
+        {
+            if (obj == null)
+                throw ThrowHelper.NotFound(typeName, id, message);
+        }
+    }
+}
