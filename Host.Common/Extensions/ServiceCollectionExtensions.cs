8ecff5af90969c5b62bae32e45f109529be7dc84
Alex Noddings <alexnoddings@users.noreply.github.com>
2020-09-29 11:09:40 +0100

    Add service collection extensions to register repositories.

---
+++ Host.Common/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Quibble.Host.Common.Repositories;
+using Quibble.Host.Common.Repositories.EntityFramework;
+
+namespace Quibble.Host.Common.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddQuibbleEntityFrameworkRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IQuizRepository, EfQuizRepository>();
+            services.AddScoped<IRoundRepository, EfRoundRepository>();
+            services.AddScoped<IQuestionRepository, EfQuestionRepository>();
+
+            return services;
+        }
+    }
+}
