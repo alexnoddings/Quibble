883b4beff24f95deff1f291118fc5d0bb14111e9
Alex Noddings <alexnoddings@users.noreply.github.com>
2020-09-29 11:09:26 +0100

    Add EntityFrameworkCore implementations for repositories.

---
+++ Host.Common/Repositories/EntityFramework/EfQuizRepository.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Quibble.Host.Common.Data;
+using Quibble.Host.Common.Data.Entities;
+using Quibble.Host.Common.Extensions;
+
+namespace Quibble.Host.Common.Repositories.EntityFramework
+{
+    internal class EfQuizRepository : IQuizRepository
+    {
+        private IQuibbleDbContext DbContext { get; }
+
+        public EfQuizRepository(IQuibbleDbContext dbContext)
+        {
+            DbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        private IQueryable<DbQuiz> Entities =>
+            DbContext.Quizzes
+                .Include(q => q.Participants)
+                    .ThenInclude(p => p.Answers)
+                .Include(q => q.Rounds)
+                    .ThenInclude(r => r.Questions)
+                        .ThenInclude(q => q.Answers)
+                            .ThenInclude(a => a.Participant);
+
+        public async Task<Guid> CreateAsync(DbQuiz quiz)
+        {
+            Ensure.NotNullOrDefault(quiz, nameof(quiz));
+            if (quiz.Id != default)
+                throw ThrowHelper.BadArgument(nameof(quiz), $"{nameof(quiz.Id)} must not be set prior to creation.");
+
+            DbContext.Quizzes.Add(quiz);
+            await DbContext.SaveChangesAsync();
+            return quiz.Id;
+        }
+
+        public Task<bool> ExistsAsync(Guid id)
+        {
+            Ensure.NotNullOrDefault(id, nameof(id));
+            return DbContext.Quizzes.AnyAsync(q => q.Id == id);
+        }
+
+        public Task<DbQuiz> GetAsync(Guid id) => GetAsync(id, Entities);
+
+        private Task<DbQuiz> GetWithoutIncludesAsync(Guid id) => GetAsync(id, DbContext.Quizzes);
+
+        private static async Task<DbQuiz> GetAsync(Guid id, IQueryable<DbQuiz> source)
+        {
+            Ensure.NotNullOrDefault(id, nameof(id));
+            DbQuiz? quiz = await source.WithIdAsync(id);
+            Ensure.Found(quiz, "Quiz", id);
+            return quiz;
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var quiz = await GetWithoutIncludesAsync(id);
+            DbContext.Quizzes.Remove(quiz);
+            await DbContext.SaveChangesAsync();
+        }
+
+        public async Task PublishAsync(Guid id)
+        {
+            var quiz = await GetWithoutIncludesAsync(id);
+            quiz.PublishedAt = DateTime.UtcNow;
+            await DbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateTitleAsync(Guid id, string newTitle)
+        {
+            var quiz = await GetWithoutIncludesAsync(id);
+            quiz.Title = newTitle;
+            await DbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ Host.Common/Repositories/EntityFramework/EfRoundRepository.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Quibble.Host.Common.Data;
+using Quibble.Host.Common.Data.Entities;
+using Quibble.Host.Common.Extensions;
+
+namespace Quibble.Host.Common.Repositories.EntityFramework
+{
+    internal class EfRoundRepository : IRoundRepository
+    {
+        private IQuibbleDbContext DbContext { get; }
+
+        public EfRoundRepository(IQuibbleDbContext dbContext)
+        {
+            DbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        private IQueryable<DbRound> Entities =>
+            DbContext.Rounds
+                .Include(r => r.Quiz)
+                .Include(r => r.Questions)
+                    .ThenInclude(q => q.Answers)
+                        .ThenInclude(a => a.Participant);
+
+        public async Task<Guid> CreateAsync(DbRound round)
+        {
+            Ensure.NotNullOrDefault(round, nameof(round));
+            if (round.Id != default)
+                throw ThrowHelper.BadArgument(nameof(round), $"{nameof(round.Id)} must not be set prior to creation.");
+            Ensure.NotNullOrDefault(round.QuizId, nameof(round.QuizId), $"{nameof(round.QuizId)} is not set.");
+
+            DbContext.Rounds.Add(round);
+            await DbContext.SaveChangesAsync();
+            return round.Id;
+        }
+
+        public Task<bool> ExistsAsync(Guid id)
+        {
+            Ensure.NotNullOrDefault(id, nameof(id));
+            return DbContext.Rounds.ExistsAsync(id);
+        }
+
+        public Task<DbRound> GetAsync(Guid id) => GetAsync(id, Entities);
+
+        private Task<DbRound> GetWithoutIncludesAsync(Guid id) => GetAsync(id, DbContext.Rounds);
+
+        private static async Task<DbRound> GetAsync(Guid id, IQueryable<DbRound> source)
+        {
+            Ensure.NotNullOrDefault(id, nameof(id));
+            DbRound? round = await source.WithIdAsync(id);
+            Ensure.Found(round, "Round", id);
+            return round;
+        }
+
+        public async Task<List<DbRound>> GetForQuizAsync(Guid quizId)
+        {
+            if (!await DbContext.Quizzes.ExistsAsync(quizId))
+                throw ThrowHelper.NotFound("Quiz", quizId);
+            return await Entities.Where(r => r.QuizId == quizId).ToListAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            DbRound round = await GetWithoutIncludesAsync(id);
+            DbContext.Rounds.Remove(round);
+            await DbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateTitleAsync(Guid id, string newTitle)
+        {
+            DbRound round = await GetWithoutIncludesAsync(id);
+            round.Title = newTitle;
+            await DbContext.SaveChangesAsync();
+        }
+    }
+}
