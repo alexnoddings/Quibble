824dec016502f16ecfa246bc5a8ea55fe3e89c11
Alex Noddings <alexnoddings@users.noreply.github.com>
2020-09-29 11:09:06 +0100

    Add extensions for entity queryables.

---
+++ Host.Common/Extensions/EntityQueryableExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Quibble.Core;
+
+namespace Quibble.Host.Common.Extensions
+{
+    internal static class EntityQueryableExtensions
+    {
+        public static Task<bool> ExistsAsync<TEntity>(this IQueryable<TEntity> source, Guid id)
+            where TEntity : IEntity<Guid> =>
+            source.AnyAsync(e => e.Id == id);
+
+        public static Task<TEntity> WithIdAsync<TEntity>(this IQueryable<TEntity> source, Guid id)
+            where TEntity : IEntity<Guid> =>
+            source.FirstOrDefaultAsync(e => e.Id == id);
+    }
+}
