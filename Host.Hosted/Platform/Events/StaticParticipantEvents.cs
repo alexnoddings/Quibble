9b6892cafeba17be234a92edc755cbf1f71173ba
Alex Noddings <alexnoddings@users.noreply.github.com>
2020-10-08 11:03:57 +0100

    Add ParticipantEvents to Hosted.

---
+++ Host.Hosted/Platform/Events/StaticParticipantEvents.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Quibble.Core.Entities;
+using Quibble.Core.Events;
+using Quibble.UI.Core.Events;
+
+namespace Quibble.Host.Hosted.Platform.Events
+{
+    internal class StaticParticipantEvents : IParticipantEvents, IParticipantEventsInvoker
+    {
+        public Task InvokeParticipantJoinedAsync(IParticipant participant) => _participantJoined.InvokeAsync(participant);
+        private static readonly AsyncEvent<IParticipant> _participantJoined = new();
+        public event Func<IParticipant, Task> ParticipantJoined
+        {
+            add => _participantJoined.Add(value);
+            remove => _participantJoined.Remove(value);
+        }
+
+        public Task InvokeParticipantLeftAsync(Guid participantId) => _participantLeft.InvokeAsync(participantId);
+        private static readonly AsyncEvent<Guid> _participantLeft = new();
+        public event Func<Guid, Task> ParticipantLeft
+        {
+            add => _participantLeft.Add(value);
+            remove => _participantLeft.Remove(value);
+        }
+    }
+}
