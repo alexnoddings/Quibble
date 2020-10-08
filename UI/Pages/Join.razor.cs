3b8f34b5f5ca66569a096c3f186cf845ad4f4145
Alex Noddings <alexnoddings@users.noreply.github.com>
2020-10-08 11:02:21 +0100

    Add temp join pages.

---
+++ UI/Pages/Join.razor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+
+namespace Quibble.UI.Pages
+{
+    [Authorize]
+    [Route("/quiz/join")]
+    public partial class Join
+    {
+        [Inject]
+        private NavigationManager NavigationManager { get; init; } = default!;
+
+        private string QuizId { get; set; } = string.Empty;
+
+        private async Task JoinAsync()
+        {
+            var id = Guid.Parse(QuizId);
+            NavigationManager.NavigateTo(JoinDirect.FormatRoute(id));
+        }
+    }
+}
---
+++ UI/Pages/JoinDirect.razor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using Quibble.UI.Core.Services;
+
+namespace Quibble.UI.Pages
+{
+    [Authorize]
+    [Route("/quiz/join/{id:guid}")]
+    public partial class JoinDirect
+    {
+        [Parameter]
+        public Guid Id { get; set; }
+
+        [Inject]
+        private IParticipantService ParticipantService { get; init; } = default!;
+
+        [Inject]
+        private NavigationManager NavigationManager { get; init; } = default!;
+
+        public static string FormatRoute(Guid quizId) => $"/quiz/join/{quizId}";
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+
+            await ParticipantService.JoinAsync(Id);
+            NavigationManager.NavigateTo(GetQuiz.FormatRoute(Id));
+        }
+    }
+}
