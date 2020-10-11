1fb8b1e281012cc6f367ec10d41863cc465c77b2
Alex Noddings <alexnoddings@users.noreply.github.com>
2020-10-11 13:53:34 +0100

    Theme (#22)
    Add themeing.

---
+++ Host.Common/Services/SimpleThemeSet.cs
@@ -0,0 +1,16 @@
+﻿using Quibble.UI.Core.Services.Theme;
+
+namespace Quibble.Host.Common.Services
+{
+    public record SimpleThemeSet : IColourSet
+    {
+        public string Text { get; init; }
+        public string Colour { get; init; }
+
+        public SimpleThemeSet(string text, string colour)
+            => (Text, Colour) = (text, colour);
+
+        public void Deconstruct(out string text, out string colour)
+            => (text, colour) = (Text, Colour);
+    }
+}
