607c6e6857ddc4a3a597f5bf8fe0ea5d3c399135
Alex Noddings <alexnoddings@users.noreply.github.com>
2020-10-14 15:05:03 +0100

    Change UseQuibbleContentRewriter to rewrite rather than redirect.

--- Host.Common/Extensions/ApplicationBuilderExtensions.cs
+++ Host.Common/Extensions/ApplicationBuilderExtensions.cs
@@ -6,24 +6,34 @@ namespace Quibble.Host.Common.Extensions
 {
     public static class ApplicationBuilderExtensions
     {
+        /// <summary>
+        ///     Adds rewrites for Quibble content.
+        ///     This <strong>must</strong> come before <c>app.UseStaticFiles()</c> so its rules can be executed before Blazors.
+        /// </summary>
+        /// <param name="app">The <see cref="IApplicationBuilder"/> to add the rewrites to.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
         public static IApplicationBuilder UseQuibbleContentRewriter(this IApplicationBuilder app)
         {
             var rwo = new RewriteOptions();
 
-            string? thisNameSpace = typeof(ApplicationBuilderExtensions).Assembly.GetName().Name;
-            rwo.AddRedirect("^favicon.ico", $"_content/{thisNameSpace}/favicon.ico");
-            rwo.AddRedirect("^css/site.css", $"_content/{thisNameSpace}/css/site.css");
-            rwo.AddRedirect("^img/bg.png", $"_content/{thisNameSpace}/img/bg.png");
+            string? thisAssemblyName = typeof(ApplicationBuilderExtensions).Assembly.GetName().Name;
+            string? executingAssemblyName = Assembly.GetEntryAssembly()?.GetName()?.Name;
 
-            rwo.AddRedirect("^css/blazorise\\.css", "_content/Blazorise/blazorise.css");
-            rwo.AddRedirect("^js/blazorise.js", "_content/Blazorise/blazorise.js");
+            rwo.AddRewrite("^favicon.ico", $"_content/{thisAssemblyName}/favicon.ico", false);
+            rwo.AddRewrite("^css/site.css", $"_content/{thisAssemblyName}/css/site.css", false);
+            rwo.AddRewrite("^img/bg.png", $"_content/{thisAssemblyName}/img/bg.png", false);
 
-            rwo.AddRedirect("^css/blazorise.bootstrap.css", "_content/Blazorise.Bootstrap/blazorise.bootstrap.css");
-            rwo.AddRedirect("^js/blazorise.bootstrap.js", "_content/Blazorise.Bootstrap/blazorise.bootstrap.js");
+            if (executingAssemblyName != null)
+                rwo.AddRewrite("^css/styles.css", $"{executingAssemblyName}.styles.css", false);
 
-            string? assemblyName = Assembly.GetEntryAssembly()?.GetName()?.Name;
-            if (assemblyName != null)
-                rwo.AddRedirect("^css/styles.css", $"{assemblyName}.styles.css");
+            // Allows for neater URLs and a more logical appearance to file structure
+            // e.g. rewrite /css/blazorise.css to /_content/blazorise/blazorise.css
+            rwo.AddRewrite("^(\\w+)/([\\w.]+).\\1", "_content/$2/$2.$1", false);
+
+            // Scoped CSS uses a relative path for imports, but styles.css is located in /css/ so we need to drop the /css/ before the _content
+            // e.g. rewrite /css/_content/Project/Project.bundle.scp.css to /_content/Project/Project.bundle.scp.css
+            //rwo.AddRewrite("^\\w+/_content/", "_content/", false);
+            rwo.AddRewrite("^css/_content/([\\w.]+)/\\1.bundle.scp.css", "_content/$1/$1.bundle.scp.css", false);
 
             app.UseRewriter(rwo);
 
