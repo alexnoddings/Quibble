cfd6bef2b358a7cc7f06f080d2fe8de4d3c23f1d
Alex Noddings <alexnoddings@users.noreply.github.com>
2020-10-20 17:27:55 +0100

    Add razor icons for states.

---
+++ UI/Features/Quiz/Icons/QuestionStateIcon.razor.cs
@@ -0,0 +1,42 @@
+﻿using Blazorise;
+using Microsoft.AspNetCore.Components;
+using Quibble.Core.Entities;
+using Quibble.UI.Core.Entities;
+using System;
+using System.Threading.Tasks;
+
+namespace Quibble.UI.Features.Quiz.Icons
+{
+    public sealed partial class QuestionStateIcon : IDisposable
+    {
+        [Parameter]
+        public SyncedQuestion Question { get; set; } = default!;
+
+        private IconName StateIconName =>
+            Question.State switch
+            {
+                QuestionState.Hidden => IconName.EyeSlash,
+                QuestionState.Visible => IconName.Eye,
+                QuestionState.Locked => IconName.Lock,
+                QuestionState.AnswerRevealed => IconName.Check,
+                _ => throw new ArgumentException($"Unexpected state {Question.State}")
+            };
+
+        protected override async Task OnInitializedAsync()
+        {
+            if (Question == null) throw new ArgumentException(nameof(Question));
+
+            await base.OnInitializedAsync();
+
+            Question.Updated += OnQuestionUpdatedAsync;
+        }
+
+        private Task OnQuestionUpdatedAsync() => InvokeAsync(StateHasChanged);
+
+        public void Dispose()
+        {
+            if (Question != null)
+                Question.Updated -= OnQuestionUpdatedAsync;
+        }
+    }
+}
