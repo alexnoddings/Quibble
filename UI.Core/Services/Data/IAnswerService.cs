3bc0b5c3c8549f247f3e0e8f3fff3c72e7e1235b
Alex Noddings <alexnoddings@users.noreply.github.com>
2020-10-20 17:24:08 +0100

    Add answer to UI core entities.

--- UI.Core/Services/Data/IAnswerService.cs
+++ UI.Core/Services/Data/IAnswerService.cs
@@ -1,12 +1,16 @@
 ﻿using Quibble.Core.Entities;
+using Quibble.UI.Core.Entities;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Quibble.UI.Core.Services.Data
 {
     public interface IAnswerService
     {
-        public Task UpdateTextAsync(Guid id, string newText);
+        public Task<List<DtoAnswer>> GetForQuizAsync(Guid quizId);
+
+        public Task UpdateTextAsync(Guid id, string newText, Guid initiatorToken);
         public Task UpdateMarkAsync(Guid id, AnswerMark newMark);
     }
 }
