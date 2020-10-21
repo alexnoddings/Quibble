7006a9fb5a5be368f642b8e2d901e7a38d048a29
Alex Noddings <alexnoddings@users.noreply.github.com>
2020-10-21 17:33:52 +0100

    Don't give users an email confirmation link on the page.

--- UI.Identity/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
+++ UI.Identity/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
@@ -1,62 +1,14 @@
 ﻿using Microsoft.AspNetCore.Authorization;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.WebUtilities;
-using Quibble.Host.Common.Data.Entities;
 
 namespace Quibble.UI.Identity.Areas.Identity.Pages.Account
 {
     [AllowAnonymous]
     public class RegisterConfirmationModel : PageModel
     {
-        private readonly UserManager<DbQuibbleUser> _userManager;
-        private readonly IEmailSender _sender;
-
-        public RegisterConfirmationModel(UserManager<DbQuibbleUser> userManager, IEmailSender sender)
+        public void OnGet()
         {
-            _userManager = userManager;
-            _sender = sender;
-        }
 
-        public string Email { get; set; } = string.Empty;
-
-        public bool DisplayConfirmAccountLink { get; set; }
-
-        public string EmailConfirmationUrl { get; set; } = string.Empty;
-
-        public async Task<IActionResult> OnGetAsync(string email, string? returnUrl = null)
-        {
-            if (email == null)
-            {
-                return RedirectToPage("/Index");
-            }
-
-            var user = await _userManager.FindByEmailAsync(email).ConfigureAwait(false);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with email '{email}'.");
-            }
-
-            Email = email;
-            // Once you add a real email sender, you should remove this code that lets you confirm the account
-            DisplayConfirmAccountLink = true;
-            if (DisplayConfirmAccountLink)
-            {
-                var userId = await _userManager.GetUserIdAsync(user).ConfigureAwait(false);
-                var code = await _userManager.GenerateEmailConfirmationTokenAsync(user).ConfigureAwait(false);
-                code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                EmailConfirmationUrl = Url.Page(
-                    "/Account/ConfirmEmail",
-                    pageHandler: null,
-                    values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
-                    protocol: Request.Scheme);
-            }
-
-            return Page();
         }
     }
 }
