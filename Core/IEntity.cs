cee96318261ace887302203fe40153f5e9abd3d3
Alex Noddings <alexnoddings@users.noreply.github.com>
2020-10-21 17:27:49 +0100

    Add docs to IEntity, made TKey covariant.

--- Core/IEntity.cs
+++ Core/IEntity.cs
@@ -3,12 +3,22 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Quibble.Core
 {
-    public interface IEntity<TKey> where TKey : IEquatable<TKey>
+    /// <summary>
+    ///     Represents an entity with an <c>Id</c>.
+    /// </summary>
+    /// <typeparam name="TKey">
+    ///     The type to use for the <c>Id</c>.
+    /// </typeparam>
+    public interface IEntity<out TKey> where TKey : IEquatable<TKey>
     {
         [Key]
         public TKey Id { get; }
     }
 
+    /// <summary>
+    ///     Represents an entity with an <c>Id</c> of type <see cref="Guid"/>.
+    /// </summary>
+    /// <inheritdoc cref="IEntity{TKey}"/>
     public interface IEntity : IEntity<Guid>
     {
     }
