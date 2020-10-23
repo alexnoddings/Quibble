524ec05bdc2249b646bfda6d7fd9b3628166d2a5
Alex Noddings <alexnoddings@users.noreply.github.com>
2020-10-23 17:49:48 +0100

    Improve info colour contrast.

--- UI.Core/Services/Theme/SimpleThemeProvider.cs
+++ UI.Core/Services/Theme/SimpleThemeProvider.cs
@@ -5,7 +5,7 @@
         public IColourSet Primary { get; } = new SimpleColourSet("#FFFFFF", "#FF5CE8");
         public IColourSet Secondary { get; } = new SimpleColourSet("#FFFFFF", "#FF5C98");
         public IColourSet Success { get; } = new SimpleColourSet("#FFFFFF", "#21A831");
-        public IColourSet Info { get; } = new SimpleColourSet("#FFFFFF", "#5CE9FF");
+        public IColourSet Info { get; } = new SimpleColourSet("#FFFFFF", "#44AAAA");
         public IColourSet Warning { get; } = new SimpleColourSet("#FFFFFF", "#FFB85C");
         public IColourSet Danger { get; } = new SimpleColourSet("#FFFFFF", "#FF725C");
         public IColourSet Light { get; } = new SimpleColourSet("#212529", "#F8F9FA"); // Bootstrap default
