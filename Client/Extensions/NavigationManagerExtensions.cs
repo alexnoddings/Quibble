a37f7cf8779c8aa8b3196d33e0b5d8cf236e6301
Alex Noddings <alexnoddings@users.noreply.github.com>
2021-03-16 16:59:01 +0000

    Change GetQueryParameter to return string.

--- Client/Extensions/NavigationManagerExtensions.cs
+++ Client/Extensions/NavigationManagerExtensions.cs
@@ -9,14 +9,20 @@ namespace BlazorIdentityBase.Client.Extensions
     public static class NavigationManagerExtensions
     {
         public static T GetQueryParameter<T>(this NavigationManager navigationManager, string parameterName, T? defaultValue = default)
+        [return: NotNullIfNotNull("defaultValue")]
+        public static string? GetQueryParameter(this NavigationManager navigationManager, string parameterName, string? defaultValue = null)
         {
             var uri = navigationManager.ToAbsoluteUri(navigationManager.Uri);
 
             var stringValue = QueryHelpers.ParseQuery(uri.Query).GetValueOrDefault(parameterName, StringValues.Empty);
             if (stringValue.Equals(StringValues.Empty))
+            var value = QueryHelpers.ParseQuery(uri.Query).GetValueOrDefault(parameterName, StringValues.Empty);
+            if (value.Equals(StringValues.Empty))
                 return defaultValue;
 
             return JsonSerializer.Deserialize<T>(stringValue);
+            return value;
+        }
         }
     }
 }
