7ae7d000f960e2936553f43cd730afe27f7d2a47
Alex Noddings <alexnoddings@users.noreply.github.com>
2021-03-17 13:56:41 +0000

    Abstract authentication operations.

--- Client/Services/IdentityAuthenticationService.cs
+++ Client/Services/IdentityAuthenticationService.cs
@@ -3,7 +3,6 @@ using System.Net.Http;
 using System.Net.Http.Json;
 using System.Text.Json;
 using System.Threading.Tasks;
-using BlazorIdentityBase.Client.Shared.Authentication.Profile;
 using BlazorIdentityBase.Shared.Authentication;
 
 namespace BlazorIdentityBase.Client.Services
@@ -24,43 +23,6 @@ namespace BlazorIdentityBase.Client.Services
             _httpClient = httpClient;
         }
 
-        public async Task<AuthenticationOperation<UserInfo>> RegisterAsync(string username, string email, string password)
-        {
-            var request = new RegisterRequest { UserName = username, Email = email, Password = password };
-            var result = await _httpClient.PostAsJsonAsync(ApiBase + "Register", request);
-            var contentStream = await result.Content.ReadAsStreamAsync();
-
-            if (result.IsSuccessStatusCode)
-            {
-                var userInfo = await JsonSerializer.DeserializeAsync<UserInfo>(contentStream, DefaultDeserialisationOptions);
-                return AuthenticationOperation<UserInfo>.FromSuccess(userInfo);
-            }
-
-            var errors = await JsonSerializer.DeserializeAsync<List<string>>(contentStream, DefaultDeserialisationOptions);
-            return AuthenticationOperation<UserInfo>.FromError(errors);
-        }
-
-        public async Task<AuthenticationOperation<UserInfo>> LoginAsync(string username, string password, bool shouldRememberUser)
-        {
-            var request = new LoginRequest { UserName = username, Password = password, ShouldRememberUser = shouldRememberUser };
-            var result = await _httpClient.PostAsJsonAsync(ApiBase + "Login", request);
-            var contentStream = await result.Content.ReadAsStreamAsync();
-
-            if (result.IsSuccessStatusCode)
-            {
-                var userInfo = await JsonSerializer.DeserializeAsync<UserInfo>(contentStream, DefaultDeserialisationOptions);
-                return AuthenticationOperation<UserInfo>.FromSuccess(userInfo);
-            }
-
-            var errors = await JsonSerializer.DeserializeAsync<List<string>>(contentStream, DefaultDeserialisationOptions);
-            return AuthenticationOperation<UserInfo>.FromError(errors);
-        }
-
-        public async Task LogoutAsync()
-        {
-            await _httpClient.PostAsync(ApiBase + "Logout", null);
-        }
-
         public async Task<UserInfo> GetUserAsync()
         {
             var result = await _httpClient.GetAsync(ApiBase + "User");
@@ -68,10 +30,36 @@ namespace BlazorIdentityBase.Client.Services
             return await JsonSerializer.DeserializeAsync<UserInfo>(contentStream, DefaultDeserialisationOptions);
         }
 
-        public async Task<AuthenticationOperation> ForgotPasswordAsync(string email)
+        public Task<AuthenticationOperation<UserInfo>> RegisterAsync(string username, string email, string password) =>
+            PerformPostAsync<RegisterRequest, UserInfo>("Register", new RegisterRequest {UserName = username, Email = email, Password = password});
+
+        public Task<AuthenticationOperation<UserInfo>> LoginAsync(string username, string password, bool shouldRememberUser) =>
+            PerformPostAsync<LoginRequest, UserInfo>("Login", new LoginRequest { UserName = username, Password = password, ShouldRememberUser = shouldRememberUser });
+
+        public Task LogoutAsync() => 
+            _httpClient.PostAsync(ApiBase + "Logout", null);
+
+        public Task<AuthenticationOperation> ForgotPasswordAsync(string email) =>
+            PerformPostAsync("ForgotPassword", new ForgotPasswordRequest { Email = email });
+
+        public Task<AuthenticationOperation> ResetPasswordAsync(string email, string token, string newPassword) =>
+            PerformPostAsync("ResetPassword", new ResetPasswordRequest { Email = email, Token = token, NewPassword = newPassword });
+
+        public Task<AuthenticationOperation> ChangePasswordAsync(string currentPassword, string newPassword) =>
+            PerformPostAsync("ChangePassword", new ChangePasswordRequest { CurrentPassword = currentPassword, NewPassword = newPassword });
+
+        public Task<AuthenticationOperation> RequestChangeEmailAsync(string currentPassword, string newEmail) =>
+            PerformPostAsync("RequestChangeEmail", new RequestChangeEmailRequest { Password = currentPassword, NewEmail = newEmail });
+
+        public Task<AuthenticationOperation> ChangeEmailAsync(string newEmail, string token) =>
+            PerformPostAsync("ChangeEmail", new ChangeEmailRequest { NewEmail = newEmail, Token = token });
+
+        public Task<AuthenticationOperation> ChangeUsernameAsync(string currentPassword, string newUsername) =>
+            PerformPostAsync("ChangeUsername", new ChangeUsernameRequest {Password = currentPassword, NewUsername = newUsername});
+
+        private async Task<AuthenticationOperation> PerformPostAsync<TValue>(string endpoint, TValue value)
         {
-            var request = new ForgotPasswordRequest { Email = email };
-            var result = await _httpClient.PostAsJsonAsync(ApiBase + "ForgotPassword", request);
+            var result = await _httpClient.PostAsJsonAsync(ApiBase + endpoint, value);
 
             if (result.IsSuccessStatusCode)
                 return AuthenticationOperation.FromSuccess();
@@ -81,69 +69,19 @@ namespace BlazorIdentityBase.Client.Services
             return AuthenticationOperation.FromError(errors);
         }
 
-        public async Task<AuthenticationOperation> ResetPasswordAsync(string email, string token, string newPassword)
+        private async Task<AuthenticationOperation<TReturn>> PerformPostAsync<TValue, TReturn>(string endpoint, TValue value)
         {
-            var request = new ResetPasswordRequest { Email = email, Token = token, NewPassword = newPassword };
-            var result = await _httpClient.PostAsJsonAsync(ApiBase + "ResetPassword", request);
+            var result = await _httpClient.PostAsJsonAsync(ApiBase + endpoint, value);
+            var contentStream = await result.Content.ReadAsStreamAsync();
 
             if (result.IsSuccessStatusCode)
-                return AuthenticationOperation.FromSuccess();
+            {
+                var returnValue = await JsonSerializer.DeserializeAsync<TReturn>(contentStream, DefaultDeserialisationOptions);
+                return AuthenticationOperation<TReturn>.FromSuccess(returnValue);
+            }
 
-            var contentStream = await result.Content.ReadAsStreamAsync();
             var errors = await JsonSerializer.DeserializeAsync<List<string>>(contentStream, DefaultDeserialisationOptions);
-            return AuthenticationOperation.FromError(errors);
-        }
-
-        public async Task<AuthenticationOperation> ChangePasswordAsync(string currentPassword, string newPassword)
-        {
-            var request = new ChangePasswordRequest { CurrentPassword = currentPassword, NewPassword = newPassword };
-            var result = await _httpClient.PostAsJsonAsync(ApiBase + "ChangePassword", request);
-
-            if (result.IsSuccessStatusCode)
-                return AuthenticationOperation.FromSuccess();
-
-            var contentStream = await result.Content.ReadAsStreamAsync();
-            var errors = await JsonSerializer.DeserializeAsync<List<string>>(contentStream, DefaultDeserialisationOptions);
-            return AuthenticationOperation.FromError(errors);
-        }
-
-        public async Task<AuthenticationOperation> RequestChangeEmailAsync(string currentPassword, string newEmail)
-        {
-            var request = new RequestChangeEmailRequest { Password = currentPassword, NewEmail = newEmail };
-            var result = await _httpClient.PostAsJsonAsync(ApiBase + "RequestChangeEmail", request);
-
-            if (result.IsSuccessStatusCode)
-                return AuthenticationOperation.FromSuccess();
-
-            var contentStream = await result.Content.ReadAsStreamAsync();
-            var errors = await JsonSerializer.DeserializeAsync<List<string>>(contentStream, DefaultDeserialisationOptions);
-            return AuthenticationOperation.FromError(errors);
-        }
-
-        public async Task<AuthenticationOperation> ChangeEmailAsync(string newEmail, string token)
-        {
-            var request = new ChangeEmailRequest { NewEmail = newEmail, Token = token };
-            var result = await _httpClient.PostAsJsonAsync(ApiBase + "ChangeEmail", request);
-
-            if (result.IsSuccessStatusCode)
-                return AuthenticationOperation.FromSuccess();
-
-            var contentStream = await result.Content.ReadAsStreamAsync();
-            var errors = await JsonSerializer.DeserializeAsync<List<string>>(contentStream, DefaultDeserialisationOptions);
-            return AuthenticationOperation.FromError(errors);
-        }
-
-        public async Task<AuthenticationOperation> ChangeUsernameAsync(string currentPassword, string newUsername)
-        {
-            var request = new ChangeUsernameRequest {Password = currentPassword, NewUsername = newUsername};
-            var result = await _httpClient.PostAsJsonAsync(ApiBase + "ChangeUsername", request);
-
-            if (result.IsSuccessStatusCode)
-                return AuthenticationOperation.FromSuccess();
-
-            var contentStream = await result.Content.ReadAsStreamAsync();
-            var errors = await JsonSerializer.DeserializeAsync<List<string>>(contentStream, DefaultDeserialisationOptions);
-            return AuthenticationOperation.FromError(errors);
+            return AuthenticationOperation<TReturn>.FromError(errors);
         }
     }
 }
