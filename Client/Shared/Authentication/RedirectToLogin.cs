3142b76673f41253e16dd2621836437192a7670c
Alex Noddings <alexnoddings@users.noreply.github.com>
2021-03-17 20:39:03 +0000

    Don't add to page history when redirecting to login.

---
+++ Client/Shared/Authentication/RedirectToLogin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BlazorIdentityBase.Client.Extensions;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace BlazorIdentityBase.Client.Shared.Authentication
+{
+    public class RedirectToLogin : ComponentBase
+    {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        [Inject]
+        public IJSRuntime JsRuntime { get; set; }
+
+        protected override void OnInitialized() =>
+            // Using NavigateTo instead adds the current page to the history.
+            // This means trying to go back will load this page and immediately navigate forwards again.
+            JsRuntime.InvokeVoidAsync("blazorInterop.setLocation", "/auth/login?returnUrl=" + Uri.EscapeDataString(NavigationManager.GetRelativeUrl()));
+
+    }
+}
