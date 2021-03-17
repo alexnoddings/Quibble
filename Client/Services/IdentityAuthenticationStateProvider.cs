4299a501ca4963a9f466eab49c4a7daafdc89011
Alex Noddings <alexnoddings@users.noreply.github.com>
2021-03-17 09:59:01 +0000

    Add ForgotPassword and ResetPassword to auth state provider.

--- Client/Services/IdentityAuthenticationStateProvider.cs
+++ Client/Services/IdentityAuthenticationStateProvider.cs
@@ -16,20 +16,10 @@ namespace BlazorIdentityBase.Client.Services
             _authenticationService = authenticationService;
         }
 
-        public async Task<AuthenticationOperation> LoginAsync(string username, string password, bool shouldRememberUser)
-        {
-            var operation = await _authenticationService.LoginAsync(username, password, shouldRememberUser);
-            if (operation.WasSuccessful)
-            {
-                _userInfo = null;
-                NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
-            }
-
-            // The operation is returned without the <TResult> as claims aren't populated in login/register requests.
-            // These will be populated later by GetAuthenticationStateAsync.
-            return operation;
-        }
-
+        /// <remarks>
+        /// The operation is returned without the &lt;TResult&gt; as claims aren't populated in login/register requests.
+        ///  These are populated by GetAuthenticationStateAsync.
+        /// </remarks>
         public async Task<AuthenticationOperation> RegisterAsync(string username, string email, string password)
         {
             var operation = await _authenticationService.RegisterAsync(username, email, password);
@@ -44,11 +34,46 @@ namespace BlazorIdentityBase.Client.Services
             return operation;
         }
 
+        /// <remarks>
+        /// The operation is returned without the &lt;TResult&gt; as claims aren't populated in login/register requests.
+        ///  These are populated by GetAuthenticationStateAsync.
+        /// </remarks>
+        public async Task<AuthenticationOperation> LoginAsync(string username, string password, bool shouldRememberUser)
+        {
+            var operation = await _authenticationService.LoginAsync(username, password, shouldRememberUser);
+            if (operation.WasSuccessful)
+            {
+                _userInfo = null;
+                NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+            }
+
+            return operation;
+        }
+
         public async Task LogoutAsync()
         {
             await _authenticationService.LogoutAsync();
             _userInfo = null;
             NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        }
+
+        public Task<AuthenticationOperation> ForgotPasswordAsync(string email) => 
+            _authenticationService.ForgotPasswordAsync(email);
+
+        /// <remarks>
+        /// The operation is returned without the &lt;TResult&gt; as claims aren't populated in login/register requests.
+        ///  These are populated by GetAuthenticationStateAsync.
+        /// </remarks>
+        public async Task<AuthenticationOperation> ResetPasswordAsync(string email, string token, string newPassword)
+        {
+            var operation = await _authenticationService.ResetPasswordAsync(email, token, newPassword);
+            if (operation.WasSuccessful)
+            {
+                _userInfo = null;
+                NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+            }
+
+            return operation;
         }
 
         public override async Task<AuthenticationState> GetAuthenticationStateAsync()
