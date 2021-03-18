90d48433890c0890c39fe3e04dc3353a1709690f
Alex Noddings <alexnoddings@users.noreply.github.com>
2021-03-18 10:07:44 +0000

    Revert using history.replaceState as it has compatibility issues.

--- Client/Shared/Authentication/RedirectToLogin.cs
+++ Client/Shared/Authentication/RedirectToLogin.cs
@@ -14,9 +14,6 @@ namespace BlazorIdentityBase.Client.Shared.Authentication
         public IJSRuntime JsRuntime { get; set; }
 
         protected override void OnInitialized() =>
-            // Using NavigateTo instead adds the current page to the history.
-            // This means trying to go back will load this page and immediately navigate forwards again.
-            JsRuntime.InvokeVoidAsync("blazorInterop.setLocation", "/auth/login?returnUrl=" + Uri.EscapeDataString(NavigationManager.GetRelativeUrl()));
-
+            NavigationManager.NavigateTo("/auth/login?returnUrl=" + Uri.EscapeDataString(NavigationManager.GetRelativeUrl()));
     }
 }
