7cda1297f2658990da87f8ebc98c08b4c0fbb367
Alex Noddings <alexnoddings@users.noreply.github.com>
2021-03-19 21:46:02 +0000

    Add auth form component.

---
+++ Client/Components/Authentication/AuthForm.razor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace Quibble.Client.Components.Authentication
+{
+    public partial class AuthForm
+    {
+        [Parameter]
+        public RenderFragment Title { get; set; } = default!;
+
+        [Parameter]
+        public RenderFragment Form { get; set; } = default!;
+
+        [Parameter]
+        public RenderFragment Submit { get; set; } = default!;
+
+        [Parameter]
+        public RenderFragment? Footer { get; set; }
+
+        [Parameter]
+        public List<string>? Errors { get; set; } = default!;
+
+        [Parameter]
+        public object Model { get; set; } = default!;
+
+        [Parameter]
+        public EventCallback<EditContext> OnSubmit { get; set; } = default!;
+
+        private bool _isSubmitting;
+
+        private async Task OnSubmitting(EditContext? editContext)
+        {
+            _isSubmitting = true;
+
+            try
+            {
+                await OnSubmit.InvokeAsync(editContext);
+            }
+            catch (Exception)
+            {
+                _isSubmitting = false;
+                StateHasChanged();
+                throw;
+            }
+
+            _isSubmitting = false;
+        }
+    }
+}
