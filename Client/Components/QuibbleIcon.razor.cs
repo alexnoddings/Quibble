82fcd1b5f1fb2a57a0bd7c875220cac9d8ea1045
Alex Noddings <alexnoddings@users.noreply.github.com>
2021-03-19 21:41:45 +0000

    Add icon component.

---
+++ Client/Components/QuibbleIcon.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Quibble.Client.Components
+{
+    public partial class QuibbleIcon
+    {
+        [Parameter]
+        public int Size { get; set; } = 48;
+    }
+}
