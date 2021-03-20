f1f91a7f917c21ef835252782912f18a9248ea7d
Alex Noddings <alexnoddings@users.noreply.github.com>
2021-03-20 11:23:17 +0000

    Update PageTitle to add standard suffix.

--- Client/Components/PageTitle.cs
+++ Client/Components/PageTitle.cs
@@ -4,21 +4,24 @@ using Microsoft.AspNetCore.Components.Web.Extensions.Head;
 
 namespace Quibble.Client.Components
 {
-    // Both the Web Head and Blazorise packages both implement a Title component.
-    // It is unreasonable to never import Blazorise and do Blazorise.ComponentName everywhere,
-    // and the Razor compiler doesn't currently understand `@using Alias = Name.Space` directives.
-    // As such, this component simply wraps the Head's Title component. This should be removed and
-    // An alias should be used instead whenever this issue is fixed.
-    // See https://github.com/dotnet/aspnetcore/issues/13090 for the issue tracker.
     public class PageTitle : ComponentBase
     {
+        private const string TitleSuffix = "Quibble";
+        private const string TitleBreak = " · ";
+
         [Parameter] 
-        public string Value { get; set; } = string.Empty;
+        public string? Value { get; set; }
 
         protected override void BuildRenderTree(RenderTreeBuilder builder)
         {
+            string pageTitle;
+            if (string.IsNullOrEmpty(Value))
+                pageTitle = TitleSuffix;
+            else
+                pageTitle = Value + TitleBreak + TitleSuffix;
+
             builder.OpenComponent<Title>(0);
-            builder.AddAttribute(1, "Value", Value);
+            builder.AddAttribute(1, "Value", pageTitle);
             builder.CloseComponent();
         }
     }
