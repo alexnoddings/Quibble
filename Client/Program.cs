677edb28ffd3451e57b311a651d2855c3f864657
Alex Noddings <alexnoddings@users.noreply.github.com>
2021-03-27 14:00:15 +0000

    Add startup class for client.

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,15 +1,5 @@
-using System;
-using System.Net.Http;
-using System.Reflection;
-using System.Text.Json;
 using System.Threading.Tasks;
-using Blazorise;
-using Blazorise.Bootstrap;
-using Fluxor;
-using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Quibble.Client.Services.Authentication;
 
 namespace Quibble.Client
 {
@@ -20,26 +10,8 @@ namespace Quibble.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddOptions();
-            builder.Services.AddAuthorizationCore();
-            builder.Services.AddScoped<IdentityAuthenticationStateProvider>();
-            builder.Services.AddScoped<AuthenticationStateProvider>(services => services.GetRequiredService<IdentityAuthenticationStateProvider>());
-            builder.Services.AddScoped<IIdentityAuthenticationService, IdentityAuthenticationService>();
-
-            builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-            builder.Services.AddOptions<JsonSerializerOptions>().Configure(o => o.PropertyNameCaseInsensitive = true);
-
-            builder.Services
-                .AddBlazorise(options =>
-                {
-                    options.ChangeTextOnKeyPress = true;
-                    options.DelayTextOnKeyPress = true;
-                    options.DelayTextOnKeyPressInterval = 80;
-                })
-                .AddBootstrapProviders();
-
-            builder.Services.AddFluxor(options => options.ScanAssemblies(Assembly.GetExecutingAssembly()));
+            var startup = new Startup(builder.HostEnvironment);
+            startup.ConfigureServices(builder.Services);
 
             await builder.Build().RunAsync();
         }
---
+++ Client/Startup.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net.Http;
+using System.Reflection;
+using System.Text.Json;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Fluxor;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Quibble.Client.Services.Authentication;
+
+namespace Quibble.Client
+{
+    public class Startup
+    {
+        private readonly IWebAssemblyHostEnvironment _environment;
+
+        public Startup(IWebAssemblyHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOptions();
+
+            ConfigureAuthorisation(services);
+            ConfigureApi(services);
+
+            services
+                .AddBlazorise(options =>
+                {
+                    options.ChangeTextOnKeyPress = true;
+                    options.DelayTextOnKeyPress = true;
+                    options.DelayTextOnKeyPressInterval = 80;
+                })
+                .AddBootstrapProviders();
+
+            services.AddFluxor(options =>
+            {
+                options.UseRouting();
+                options.ScanAssemblies(Assembly.GetExecutingAssembly());
+
+                if (_environment.IsDevelopment())
+                    options.UseReduxDevTools();
+            });
+        }
+
+        private void ConfigureAuthorisation(IServiceCollection services)
+        {
+            services.AddAuthorizationCore();
+            services.AddScoped<IdentityAuthenticationStateProvider>();
+            services.AddScoped<AuthenticationStateProvider>(serviceProvider => serviceProvider.GetRequiredService<IdentityAuthenticationStateProvider>());
+            services.AddScoped<IIdentityAuthenticationService, IdentityAuthenticationService>();
+        }
+
+        private void ConfigureApi(IServiceCollection services)
+        {
+            services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(_environment.BaseAddress) });
+            services.AddOptions<JsonSerializerOptions>().Configure(o => o.PropertyNameCaseInsensitive = true);
+        }
+    }
+}
