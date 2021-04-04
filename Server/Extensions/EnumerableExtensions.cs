bc1bab8aac1355bc0725bcdd7b6c8f3ecdf43fd6
Alex Noddings <alexnoddings@users.noreply.github.com>
2021-04-04 10:27:38 +0100

    Add helper extensions.

---
+++ Server/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Quibble.Server.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static bool None<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            foreach (TSource element in source)
+                if (predicate(element))
+                    return false;
+
+            return true;
+        }
+    }
+}
---
+++ Server/Extensions/QueryableEntityExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Quibble.Shared.Models;
+
+namespace Quibble.Server.Extensions
+{
+    public static class QueryableEntityExtensions
+    {
+        public static Task<TEntity> FindAsync<TId, TEntity>(this IQueryable<TEntity> source, TId id)
+            where TId : IEquatable<TId>
+            where TEntity : IEntity<TId> =>
+            source.FirstOrDefaultAsync(entity => entity.Id.Equals(id));
+    }
+}
