e99fe714ca22b0a5155d00e5bbcbf65a01ff5ea0
Alex Noddings <alexnoddings@users.noreply.github.com>
2021-05-03 20:35:04 +0100

    Add synced text editor component.

---
+++ Client/Components/SynchronisedTextEditor.razor.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading.Tasks;
+using Blazorise;
+using Microsoft.AspNetCore.Components;
+using Quibble.Client.Sync.DelayedExecution;
+
+namespace Quibble.Client.Components
+{
+    public partial class SynchronisedTextEditor : IAsyncDisposable
+    {
+        [Parameter]
+        public string Value { get; set; } = string.Empty;
+
+        private string PreviousValue { get; set; } = string.Empty;
+
+        private string LocalValue { get; set; } = string.Empty;
+
+        [Parameter]
+        public Func<string, Task> SaveFunction { get; set; } = default!;
+
+        private Debouncer<string> SaveDebouncer { get; } = new(1000);
+
+        [Parameter]
+        public Func<string, Task>? PreviewFunction { get; set; }
+
+        private Throttler<string> PreviewThrottler { get; } = new(333);
+
+        [Parameter]
+        public Size Size { get; set; } = Size.None;
+
+        protected override void OnParametersSet()
+        {
+            base.OnParametersSet();
+
+            if (PreviousValue != Value)
+            {
+                PreviousValue = Value;
+                LocalValue = Value;
+            }
+
+            SaveDebouncer.Executor = SaveFunction;
+            PreviewThrottler.Executor = PreviewFunction;
+        }
+
+        private void OnTextChanged(string newValue)
+        {
+            LocalValue = newValue;
+            PreviewThrottler.Invoke(newValue);
+            SaveDebouncer.Invoke(newValue);
+        }
+
+        private async Task OnBlurAsync()
+        {
+            await PreviewThrottler.FlushAsync();
+            await SaveDebouncer.FlushAsync();
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await PreviewThrottler.FlushAsync();
+            await SaveDebouncer.FlushAsync();
+        }
+    }
+}
