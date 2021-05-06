61143b2951fe509b8737d09b43f88bff63748f19
Alex Noddings <alexnoddings@users.noreply.github.com>
2021-05-06 16:21:04 +0100

    Add synced editor for question points.

---
+++ Client/Components/SynchronisedEdit/SynchronisedPointsEdit.razor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Quibble.Client.Components.SynchronisedEdit
+{
+    public partial class SynchronisedPointsEdit
+    {
+        [Parameter]
+        public decimal Points { get; set; }
+
+        private string LocalPointsString { get; set; } = string.Empty;
+
+        [Parameter]
+        public Func<decimal, Task> SaveFunction { get; set; } = default!;
+
+        protected override void OnParametersSet()
+        {
+            base.OnParametersSet();
+            LocalPointsString = Points.ToString("G4");
+        }
+
+        private Task RoundAndUpdatePointsAsync()
+        {
+            if (!decimal.TryParse(LocalPointsString, out decimal points))
+            {
+                LocalPointsString = Points.ToString("G4");
+                return Task.CompletedTask;
+            }
+
+            // Ensure points are a division of 0.25
+            points = Math.Round(points * 4, MidpointRounding.ToEven) / 4;
+            points = Math.Clamp(points, 0.25m, 10m);
+
+            LocalPointsString = points.ToString("G4");
+            return SaveFunction(points);
+        }
+    }
+}
