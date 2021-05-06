145a4221893efeb4f8939a7ede373c200088aa5f
Alex Noddings <alexnoddings@users.noreply.github.com>
2021-05-06 15:17:36 +0100

    Add options modal.

---
+++ Client/Components/Confirmation/OptionsModalContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Quibble.Client.Components.Confirmation
+{
+    public class OptionsModalContext<TValue>
+    {
+        private Action<TValue?> ChooseAction { get; }
+
+        public OptionsModalContext(Action<TValue?> chooseAction)
+        {
+            ChooseAction = chooseAction;
+        }
+
+        public void Choose(TValue? value) => ChooseAction(value);
+    }
+}
