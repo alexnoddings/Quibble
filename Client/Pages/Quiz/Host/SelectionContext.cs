845efca3ae02291bd90f43986d0d4383d3207912
Alex Noddings <alexnoddings@users.noreply.github.com>
2021-05-16 13:37:17 +0100

    Add question list for host mode, context for the selected question.

---
+++ Client/Pages/Quiz/Host/SelectionContext.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading.Tasks;
+using Quibble.Client.Sync.Entities.HostMode;
+
+namespace Quibble.Client.Pages.Quiz.Host
+{
+    public class SelectionContext
+    {
+        private ISynchronisedHostModeQuiz Quiz { get; }
+        public ISynchronisedHostModeRound Round => Quiz.Rounds[RoundNumber];
+        public ISynchronisedHostModeQuestion Question => Round.Questions[QuestionNumber];
+
+        public int RoundNumber { get; private set; }
+        public int QuestionNumber { get; private set; }
+
+        public event Func<Task>? Updated;
+
+        public bool IsAtFirstQuestion => RoundNumber == 0 && QuestionNumber == 0;
+        public bool IsAtLastQuestion => RoundNumber == Quiz.Rounds.Count - 1 && QuestionNumber == Round.Questions.Count - 1;
+
+        public SelectionContext(ISynchronisedHostModeQuiz quiz)
+        {
+            Quiz = quiz;
+        }
+
+        public Task UpdateSelectionAsync(int roundNumber, int questionNumber)
+        {
+            RoundNumber = Math.Clamp(roundNumber, 0, Quiz.Rounds.Count - 1);
+            QuestionNumber = Math.Clamp(questionNumber, 0, Round.Questions.Count - 1);
+
+            return OnUpdatedAsync();
+        }
+
+        public Task MoveToPreviousQuestionAsync()
+        {
+            if (RoundNumber == 0 && QuestionNumber == 0)
+                return Task.CompletedTask;
+
+            if (QuestionNumber == 0)
+            {
+                RoundNumber--;
+                QuestionNumber = Round.Questions.Count - 1;
+            }
+            else
+            {
+                QuestionNumber--;
+            }
+
+            return OnUpdatedAsync();
+        }
+
+        public Task MoveToNextQuestionAsync()
+        {
+            if (RoundNumber == Quiz.Rounds.Count - 1 && QuestionNumber == Round.Questions.Count - 1)
+                return Task.CompletedTask;
+
+            if (QuestionNumber == Round.Questions.Count - 1)
+            {
+                QuestionNumber = 0;
+                RoundNumber++;
+            }
+            else
+            {
+                QuestionNumber++;
+            }
+
+            return OnUpdatedAsync();
+        }
+
+        private Task OnUpdatedAsync() => Updated?.Invoke() ?? Task.CompletedTask;
+    }
+}
