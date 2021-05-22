857efa4adebe860fe9e4ea985817dd94b790fbf1
Alex Noddings <alexnoddings@users.noreply.github.com>
2021-05-22 14:47:34 +0100

    Add take mode entity interfaces.

---
+++ Client.Synchronisation/Entities/TakeMode/ISynchronisedTakeModeQuiz.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Quibble.Client.Sync.Entities.TakeMode
+{
+    public interface ISynchronisedTakeModeQuiz : ISynchronisedQuiz
+    {
+        public event Func<ISynchronisedTakeModeRound, Task>? RoundAdded;
+
+        public IReadOnlyList<ISynchronisedTakeModeRound> Rounds { get; }
+        public IReadOnlyList<ISynchronisedTakeModeParticipant> Participants { get; }
+    }
+}
