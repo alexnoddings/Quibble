3da73310285ba89d7419bc1d8f499be2e2c3e2b0
Alex Noddings <alexnoddings@users.noreply.github.com>
2021-06-05 18:26:40 +0100

    Add debug display when not in prod.

---
+++ Client/Components/DebugDisplay.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace Quibble.Client.Components
+{
+    public partial class DebugDisplay
+    {
+        [Inject]
+        private IWebAssemblyHostEnvironment HostEnvironment { get; set; } = default!;
+
+        [Inject]
+        private IConfiguration Configuration { get; set; } = default!;
+    }
+}
