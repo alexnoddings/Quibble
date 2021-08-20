4a3c9b7e7e4a51b91799c4619e7a4866c8a983fe
Alex Noddings <alexnoddings@users.noreply.github.com>
2021-08-20 14:42:49 +0100

    Remove unused dependency.

--- Client/Components/FullPageTitle.cs
+++ Client/Components/FullPageTitle.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Rendering;
 using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.Web.Extensions.Head;
 
 namespace Quibble.Client.Components
 {
